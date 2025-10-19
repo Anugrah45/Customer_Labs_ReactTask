16d704a73d2030cf96fcbd0bef6cc7536f923090
Anugrah45 <anugrahm326@gmail.com>
2025-10-19 12:47:51 +0530

    Done customer labs task

--- machine_test/src/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import ProcedureReducer from './slices/procedureSlice'
-
-
-export const store=configureStore({
-    reducer:{
-        procedures:ProcedureReducer
-    }
-})
